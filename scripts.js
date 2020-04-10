f75cde0944f4f261511c4a09da6ee55c7475ca53
morgancostigan <morgan.costigan@gmail.com>
2020-04-10 14:04:34 -0500

    alternate togglePlay function

--- scripts.js
+++ scripts.js
@@ -8,13 +8,18 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 //BUILD OUT FUNCTIONS
+// function togglePlay() {
+//     if(video.paused){ //.paused is a built in video property
+//         video.play();
+//     } else {
+//         video.pause();
+//     }
+// }//end togglePlay function
+
 function togglePlay() {
-    if(video.paused){ //.paused is a built in video property
-        video.play();
-    } else {
-        video.pause();
-    }
-}//end togglePlay function
+    const method = video.paused ? 'play' : 'pause'; //uses ?, the conditional or ternary operator
+    video[method]();
+}//end togglePlay function v2
 
 
 /* HOOK UP EVENT LISTENERS */ 
