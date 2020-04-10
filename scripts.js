f3f51ddbf84c976241c9fa8ac08c1dc5387409ce
morgancostigan <morgan.costigan@gmail.com>
2020-04-10 13:53:24 -0500

    all elements now in script.js file

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//GET OUR ELEMENTS
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+//BUILD OUT FUNCTIONS
+
+
+/* HOOK UP EVENT LISTENERS */ 
